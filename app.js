971491145e82db8db5705058cb628d3415a9faaf
Harsh Patil <jharshpatil@gmail.com>
2026-03-18 23:22:08 +0530

    structure done

--- app.js
+++ app.js
@@ -3,8 +3,40 @@ let inp = document.querySelector("input");
 let ul = document.querySelector("ul")
 
 list_element.addEventListener("click", () => {
-    let item = document.createElement("li");
-    item.innerText = inp.value;
-    ul.appendChild(item);
-    inp.value = "";
-})
+   let li = document.createElement("li");
+
+   let checkbox = document.createElement("input");
+   checkbox.type = "checkbox"
+   checkbox.classList.add("check-box");
+
+   let span = document.createElement("span");
+   span.innerText = inp.value;
+   span.classList.add("task");
+
+   let span2 = document.createElement("span");
+   span2.classList.add("del-icon");
+
+
+   let delBtn = document.createElement("i");
+   delBtn.classList.add("delete");
+
+   delBtn.innerHTML = '<i class="fa-solid fa-x"></i>';
+
+   let line = document.createElement("div");
+   line.classList.add("line2");
+
+   delBtn.addEventListener("click", () => {
+    li.remove();
+});
+
+   span2.appendChild(delBtn);
+   li.appendChild(checkbox);
+   li.appendChild(span);
+   li.appendChild(span2);
+//    card.appendChild(line2);
+
+   ul .appendChild(li);
+
+   inp.value = "";
+});
+
